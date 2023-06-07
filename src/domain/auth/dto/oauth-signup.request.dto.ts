af1d3f96b77fdfff679e9c7e44120abe1c41f45f
inhyeok <ss112d86@gmail.com>
2023-06-07 22:46:52 +0900

    feat: 요청값 검증 로직 추가

--- src/domain/auth/dto/oauth-signup.request.dto.ts
+++ src/domain/auth/dto/oauth-signup.request.dto.ts
@@ -1,8 +1,30 @@
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import { IsInt, IsString, Matches } from 'class-validator';
+
 export class OAuthSignUpRequestDto {
+  @IsString()
   id_token: string;
+
+  @IsString()
   access_token: string;
+
+  @IsString()
+  @Matches(/^[a-z가-힣0-9]{2,6}$/, { message: '닉네임 값을 확인해주세요' })
   nickname: string;
+
+  @IsString()
+  @Transform(({ value, obj }) => {
+    if (value === 'M' || value === 'F') {
+      return value;
+    }
+    throw new BadRequestException('성별 값을 확인해주세요!');
+  })
   gender: string;
+
+  @IsString()
   birth: string;
+
+  @IsInt()
   age: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { swaggerConfig } from 'src/global/configs/swagger.config';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './global/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
   const configService = app.get(ConfigService);
